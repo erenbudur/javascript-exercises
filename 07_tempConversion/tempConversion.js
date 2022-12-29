fd44461e5ba3baf7d0c0e72ab646f645730122f0
erenbudur <56324781+erenbudur@users.noreply.github.com>
2022-12-29 09:02:46 -0500

    Complete Ex-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const round = function(n,d){
+  return Math.round(n *(10**d)) / (10**d)
+}
+const convertToCelsius = function(f) {
+
+  return round((f-32)*(5/9),1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return round(c*(9/5)+32,1)
 };
+
+
 
 // Do not edit below this line
 module.exports = {
