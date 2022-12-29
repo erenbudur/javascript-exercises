441d87d030ab870ae72f45434264832e5df2dd46
erenbudur <56324781+erenbudur@users.noreply.github.com>
2022-12-29 05:41:04 -0500

    Complete Ex-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(s,n) {
+    if (n<0){
+        return "ERROR"
+    }
+    let sf =""
+    for (let i = 0;i<n;i++){
 
+        sf +=  s
+    }
+    return sf
 };
 
 // Do not edit below this line
