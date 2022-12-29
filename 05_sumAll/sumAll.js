eb1a9d7f49dfa81f9e3a04e934ce01b56a506c28
erenbudur <56324781+erenbudur@users.noreply.github.com>
2022-12-29 06:39:04 -0500

    Complete Ex-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(n1,n2) {
+    if (!(typeof n1 == "number" && typeof n2 == "number") || n1<0||n2<0)
+    {
+        return "ERROR"
+    }
+    let larger = n1>n2 ? n1 : n2;
+    let smaller = n1<n2 ? n1 : n2;
 
+    let fsum = 0;
+    for(;smaller<=larger;smaller++){
+        fsum+=smaller
+    }
+    return fsum
 };
 
 // Do not edit below this line
