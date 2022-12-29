2f0f33df77229dc1ca47b1dc4f1b1b8aca4cc968
erenbudur <56324781+erenbudur@users.noreply.github.com>
2022-12-29 06:13:22 -0500

    Compelte Ex-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...vals) {
+    let valArr = [...vals]
+    let newArr = []
+   
+    for(let i = 0;i<arr.length;i++){
+        if(!valArr.includes(arr[i])){
+            newArr.push(arr[i])
+        }
 
+    }
+    return newArr
 };
 
 // Do not edit below this line
