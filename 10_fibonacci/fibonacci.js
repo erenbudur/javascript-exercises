5f30c4cb18dce188095c2d402d0d2ef869ccbdaf
erenbudur <56324781+erenbudur@users.noreply.github.com>
2023-01-12 04:56:36 -0500

    Complete Ex-10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    if (n < 0) {
+return "OOPS"
+    }
+    let acc = 1
+    let val = 1
+    for (let i = 0; i < n - 1; i++) {
+        let temp = val;
+        val = val + acc;
+        acc = temp;
+    }
+    return acc
 };
 
 // Do not edit below this line
