f2c61b5e89ee8dd557d2fa22143400801ae64a64
erenbudur <56324781+erenbudur@users.noreply.github.com>
2023-01-12 05:23:42 -0500

    Complete Ex-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    return people.reduce((oldest,current)=>{
+        return getAge(oldest.yearOfBirth,oldest.yearOfDeath)>getAge(current.yearOfBirth,current.yearOfDeath)? oldest:current
+
+
+    })
 };
+
+function getAge(b,d){
+    if(!d){
+        d = new Date().getFullYear()
+    }
+    return d-b
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
